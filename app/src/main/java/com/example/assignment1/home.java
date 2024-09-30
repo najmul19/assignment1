3178bc54dc7203720838ca6e85fe78e2417e394a
najmul <mdnajmulislam10992@gmail.com>
2024-09-30 16:51:17 +0600

    p commit

--- app/src/main/java/com/example/assignment1/home.java
+++ app/src/main/java/com/example/assignment1/home.java
@@ -4,15 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class home extends AppCompatActivity {
-    //change 1git
-    // change 2
 
     private Button btnText,btnImage,btnCheckRadio,btnSeekSwitchRatting,formANdRegexValidation;
     @Override
